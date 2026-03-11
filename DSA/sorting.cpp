ecf867b22a38560d5a5828d90b48f8919d9ade1a
Ashiii27 <ashiii.2790@gmail.com>
2026-03-11 13:31:24 +0530

    bubble sort

--- DSA/sorting.cpp
+++ DSA/sorting.cpp
@@ -14,6 +14,17 @@ void selectionSort(int n, int arr[]) {
     }
 }
 
+void bubble_sort(int n, int arr[]) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+
 int main() {
     int n;
     cout << "enter the size of array : ";
@@ -25,7 +36,8 @@ int main() {
     for (int i = 0; i < n; i++)
         cin >> arr[i];
 
-    selectionSort(n, arr);
+    //selectionSort(n, arr);
+    bubble_sort(n, arr);
 
     cout << "sorted array: ";
     for (int i = 0; i < n; i++)
