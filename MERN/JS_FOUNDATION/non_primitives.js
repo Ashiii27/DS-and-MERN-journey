53544f58fd5781e87d1544e75ff4654b23fcdc64
Ashiii27 <ashiii.2790@gmail.com>
2026-03-14 09:15:00 +0530

    non-primitives functions

--- MERN/JS_FOUNDATION/non_primitives.js
+++ MERN/JS_FOUNDATION/non_primitives.js
@@ -8,10 +8,28 @@ const username = {
   isLoggedIn: "true",
 };
 
-username.firstName = "Mr.H";
-username.lastname = "K";
-console.log(username.firstName);
-console.log(username.lastname);
+// username.firstName = "Mr.H";
+// username.lastname = "K";
+// console.log(username.firstName);
+// console.log(username.lastname);
+// console.log(typeof(username));
 
-console.log(typeof(username));
+let today=new Date();
+//console.log(today);  //inbuilt object
+
+// Array
+
+// let numbers = [1, 2, 3, 4, 5, "a", "b", true];
+// console.log(numbers);
+
+let name = ["Ashish", "Kumar"];
+//console.log(name[0]);
+
+//console.log(1+"1")  //implicit type conversion in JavaScript
+let isValue = true;
+console.log(isValue + 1);
+console.log(isValue+"1");
+
+
+
 
